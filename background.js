6fe08b6789c08df06ca661a3cd3d0e84e1924ae1
corbindavenport <davenportcorbin@gmail.com>
2014-01-17 23:10:36 -0500

    added omnibar search
    now supports searching Wikipedia from the chrome omnibar, search
    results and recommendations are pulled from existing language setting

---
+++ background.js
@@ -0,0 +1,102 @@
+/*
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+*/
+
+// Context Menu Search
+
+function onSearch(info, tab) {
+        chrome.tabs.detectLanguage(null, function(lang) {
+            var language = localStorage["launguage"];
+           var taburl = "http://" + language + ".wikipedia.org/w/index.php?title=Special:Search&search=" + info.selectionText.replace(/\s/g, "+");
+             chrome.tabs.create({ url: taburl, selected: false });
+        });
+    }
+    var id = chrome.contextMenus.create({ "title": "Wikipedia Search", "contexts": ["selection"],
+        "onclick": onSearch
+    });
+
+// Omnibox Search
+// Derived from OmniWiki (github.com/hamczu/OmniWiki)
+
+var language = localStorage["launguage"];
+var currentRequest = null;
+
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+    if (currentRequest != null) {
+        currentRequest.onreadystatechange = null;
+        currentRequest.abort();
+        currentRequest = null;
+    }        
+
+    updateDefaultSuggestion(text);
+    
+    if(text.length > 0){
+        currentRequest = suggests(text, function(data) {
+            var results = [];
+
+            for(var i = 0; i < data[1].length; i++){
+                results.push({
+                    content: data[1][i],
+                    description: data[1][i]
+                });
+            }
+
+            suggest(results);
+        });
+    } else {
+     
+    }
+});
+
+function resetDefaultSuggestion() {      
+    chrome.omnibox.setDefaultSuggestion({
+        description: ' '
+    });
+}
+
+resetDefaultSuggestion();
+var searchLabel = chrome.i18n.getMessage('search_label');
+function updateDefaultSuggestion(text) {      
+    chrome.omnibox.setDefaultSuggestion({
+        description: searchLabel + ': %s'
+    });
+   
+}
+
+chrome.omnibox.onInputStarted.addListener(function() {
+    updateDefaultSuggestion('');
+});
+
+chrome.omnibox.onInputCancelled.addListener(function() {
+    resetDefaultSuggestion();
+});
+
+
+function suggests(query, callback) {
+    var req = new XMLHttpRequest();
+  
+    req.open("GET", "http://" + language + ".wikipedia.org/w/api.php?action=opensearch&namespace=0&suggest=&search=" + query, true);
+    req.onload = function(){
+        if(this.status == 200){
+            try{                  
+                callback(JSON.parse(this.responseText));
+            }catch(e){
+                this.onerror();
+            }
+        }else{
+            this.onerror();
+        }
+    };
+    req.onerror = function(){
+
+    };
+    req.send();
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+    chrome.tabs.update(null, {url: "http://" + language + ".wikipedia.org/w/index.php?search=" + text});
+})();
