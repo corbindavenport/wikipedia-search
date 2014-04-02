544bc683fe42d1a314ed7db44aa4a55e4153317e
Corbin Davenport <davenportcorbin@gmail.com>
2014-04-01 22:16:01 -0400

    added antp widget, changed version to 6.0

--- background.js
+++ background.js
@@ -19,6 +19,35 @@ chrome.runtime.onInstalled.addListener(function(details){
     chrome.tabs.create({'url': chrome.extension.getURL('welcome.html')});
 });
 
+// Awesome New Tab Page Widget
+
+var info = {
+  poke: 3,
+  width: 1,
+  height: 1,
+  path: "widget.html",
+  "v2": {
+    "resize": false,
+    "min_height": 1,
+    "max_height": 1,
+    "min_width": 1,
+    "max_width": 1
+  },
+  "v3": {
+    "multi_placement": false
+  }
+};
+
+chrome.extension.onMessageExternal.addListener(function (request, sender, sendResponse) {
+  if ( request === "mgmiemnjjchgkmgbeljfocdjjnpjnmcg-poke" ) {
+    chrome.extension.sendMessage(
+    sender.id, {
+      head: "mgmiemnjjchgkmgbeljfocdjjnpjnmcg-pokeback",
+      body: info,
+    });
+  }
+});
+
 // Context Menu Search
 
 function onSearch(info, tab) {
