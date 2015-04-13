00009677ed3e203b34c210075fc9eca262fede8b
Corbin Davenport <davenportcorbin@gmail.com>
2015-04-13 13:02:48 -0400

    uploaded wikipedia search 6.3.1

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,4 @@
-/*
+9/*
 This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
@@ -12,7 +12,7 @@ chrome.runtime.sendMessage({method: "getLocalStorage", key: "contentscripts"}, f
 	if (response.data === "on") {
 		if (window.location.href.indexOf("/wiki/") > -1) {
 			// Add extra button for Wikipedia Search on left panel
-			document.getElementById("p-navigation").innerHTML = document.getElementById("p-navigation").innerHTML + '<h3 style="display: block; padding-top: 5px; background-position: top left; background-repeat: no-repeat; background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIwAAAABCAAAAAAphRnkAAAAJ0lEQVQIW7XFsQEAIAyAMPD/b7uLWz8wS5youFW1UREfiIpH1Q2VBz7fGPS1dOGeAAAAAElFTkSuQmCC); background-image: url(//bits.wikimedia.org/static-1.25wmf24/skins/Vector/images/portal-break.png?2015-04-01T19:10:00Z)!ie;">Wikipedia Search ' + chrome.runtime.getManifest().version + '</h3><div class="body"><ul><li><a href="' + chrome.extension.getURL('settings.html') + '" title="Open Wikipedia Search extension settings in new tab" target="_blank">Extension Settings</a></li><li><a href="http://github.com/corbindavenport/wikipedia-search" title="Open Wikipedia Search extenion project on GitHub in a new tab" target="_blank">GitHub Project</a></li><li><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=4SZVSMJKDS35J&lc=US&item_name=Wikipedia%20Search%20Donation&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donate_LG%2egif%3aNonHosted" title="Donate to the Wikipedia Search extension developer via PayPal in a new tab" target="_blank">Donate via PayPal</a></li><li><a title="Donate to the Wikipedia Search extension developer via Bitcoin" target="_blank">Donate via Bitcoin</a></li><li><a href="http://github.com/corbindavenport/wikipedia-search/issues/new" title="Submit bug report through GitHub" target="_blank">Report bug</a></li></ul></div>';
+			document.getElementById("p-navigation").innerHTML = document.getElementById("p-navigation").innerHTML + '<h3 style="display: block; padding-top: 10px; background-position: top left; background-repeat: no-repeat; background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIwAAAABCAAAAAAphRnkAAAAJ0lEQVQIW7XFsQEAIAyAMPD/b7uLWz8wS5youFW1UREfiIpH1Q2VBz7fGPS1dOGeAAAAAElFTkSuQmCC); background-image: url(//bits.wikimedia.org/static-1.25wmf24/skins/Vector/images/portal-break.png?2015-04-01T19:10:00Z)!ie;">Wikipedia Search ' + chrome.runtime.getManifest().version + '</h3><div class="body"><ul><li><a href="' + chrome.extension.getURL('settings.html') + '" title="Open Wikipedia Search extension settings in new tab" target="_blank">Extension Settings</a></li><li><a href="http://github.com/corbindavenport/wikipedia-search" title="Open Wikipedia Search extenion project on GitHub in a new tab" target="_blank">GitHub Project</a></li><li><a href="http://github.com/corbindavenport/wikipedia-search/issues/new" title="Submit bug report through GitHub" target="_blank">Report bug</a></li></ul></div>';
 
 			// Add information about wikipedia search to top header
 			header = document.getElementById("p-personal").children[1];
@@ -20,10 +20,6 @@ chrome.runtime.sendMessage({method: "getLocalStorage", key: "contentscripts"}, f
 
 			// Add Wikipedia Search info to footer
 			document.getElementById("footer-info-copyright").innerHTML = document.getElementById("footer-info-copyright").innerHTML + '<br /><br />Wikipedia Search ' + chrome.runtime.getManifest().version + ' is developed by ' + chrome.runtime.getManifest().author + ' and licensed under the GPLv3 license. The source code for Wikipedia Search is freely available on its <a href="' + chrome.runtime.getManifest().homepage_url + '" target="blank_">GitHub repository</a>.';
-
-			// Bitcoin Donation
-			document.body.innerHTML = '<div id="bitcoin" style="display: none; background-color: #FFF; color: #333; font: 14px arial, sans-serif; border-bottom: 1px solid #A9A8A6; padding: 5px; width: 100%; position: fixed; z-index:500;"><b>Please send bitcoin to the following address:</b> <span style="font-family:Monaco,Menlo,Consolas,monospace;">1GSbhaUp8JSRepqQceHzrGhV1bPk9hcPuS</span></div>' + document.body.innerHTML;
-			document.querySelector('a[title="Donate to the Wikipedia Search extension developer via Bitcoin"]').onclick=function(){document.getElementById("bitcoin").style.display = "block"};
 		}
 	} else {
 		if (window.location.href.indexOf("/wiki/") > -1) {
@@ -37,8 +33,8 @@ chrome.runtime.sendMessage({method: "getLocalStorage", key: "contentscripts"}, f
 
 chrome.runtime.sendMessage({method: "getLocalStorage", key: "hidesearch"}, function(response) {
 	if (response.data === "on" && window.location.href.indexOf("/wiki/") > -1) {
-			document.getElementById("searchInput").setAttribute("disabled","disabled");
-			document.getElementById("searchInput").setAttribute("placeholder","Address Bar search enabled");
+			document.getElementById("searchform").style.display = "none";
+			document.getElementById("p-search").style.marginRight = "0";
 	} else if (response.data === "on" && document.body.querySelector(".search-form")) {
 			document.body.querySelector(".search-form").innerHTML = '<div style="background-color: #f9f9f9; border: 1px solid #aaa; margin-top: 0.5em; padding: 0.7em; width: auto;">Inline search disabled, use your browser address bar.</div>';
 		}
