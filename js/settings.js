49caf98b25c339e652e7ce2e7c474aa6921c625f
Corbin Davenport <me@corbin.io>
2017-11-24 20:10:08 -0500

    Language drop-down menu now automatically generated

--- js/settings.js
+++ js/settings.js
@@ -6,12 +6,22 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 */
 
-// List of Wikipedia's supported language in an array, for the auto-detect functionality
+// List of Wikipedia's supported language in an array
 var langArray= ["ar","az","bg","nan","be","ca","cs","da","de","et","el","en","simple","es","eo","eu","fa","fr","gl","ko","hy","hi","hr","id","it","he","ka","la","lt","hu","ms","min","nl","ja","no","nn","ce","uz","pl","pt","kk","ro","ru","ceb","sk","sl","sr","sh","fi","sv","ta","th","tr","uk","ur","vi","vo","war","zh"];
 var detailArray = ["العربية","Azərbaycanca","Български","Bân-lâm-gú / Hō-ló-oē","Беларуская (Акадэмічная)","Català","Čeština","Dansk","Deutsch","Eesti","Ελληνικά","English","Simple English","Español","Esperanto","Euskara","فارسی","Français","Galego","한국어","Հայերեն","हिन्दी","Hrvatski","Bahasa Indonesia","Italiano","עברית","ქართული","Latina","Lietuvių","Magyar","Bahasa Melayu","Bahaso Minangkabau","Nederlands","日本語","Norsk (Bokmål)","Norsk (Nynorsk)","Нохчийн","Oʻzbekcha / Ўзбекча","Polski","Português","Қазақша / Qazaqşa / قازاقشا","Română","Русский","Sinugboanong Binisaya","Slovenčina","Slovenščina","Српски / Srpski","Srpskohrvatski / Српскохрватски","Suomi","Svenska","தமிழ்","ภาษาไทย","Türkçe","Українська","اردو","Tiếng Việt","Volapük","Winaray","中文"];
 
 // Load settings
 $(window).on('load', function() {
+	// Populate languages menu
+	langArray.forEach(function callback(currentValue, index, array) {
+		$('#language').append($('<option>', {
+			value: currentValue,
+			text: detailArray[langArray.indexOf(currentValue)]
+		}));
+	});
+	// Remove loading option
+	$("#language option[value='loading'").remove();
+	// Set to current language
 	$("#language").val(localStorage["language"]);
 	if (localStorage.getItem("protocol") === "https://") {
 		$("input[name='protocol']").prop("checked", true);
@@ -35,7 +45,6 @@ $(document).on('click', ".reset-language", function() {
 		localStorage["language"] = lang;
 		localStorage["full-language"] = detailArray[langArray.indexOf(lang)];
 	} else {
-		// Set it to English as default
 		alert("Sorry, Wikipedia Search could not auto-detect your system language.")
 	}
 });
@@ -46,9 +55,9 @@ $(document).on('change', "input,select", function() {
 	localStorage["language"] = $("#language").val();
 	localStorage["full-language"] = detailArray[langArray.indexOf($("#language").val())];
 	if ($("input[name='protocol']").is(":checked")) {
-    localStorage["protocol"] = "https://";
-  } else {
-    localStorage["protocol"] = "http://";
-  }
+		localStorage["protocol"] = "https://";
+	} else {
+		localStorage["protocol"] = "http://";
+	}
 	localStorage.setItem("shortcut", $("input[name='shortcut']").is(":checked"));
 });
