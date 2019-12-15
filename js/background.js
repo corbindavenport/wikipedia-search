128ef0f748dd069aae2fe3362e9c8e7b86c29dde
Corbin Davenport <corbin@pop-os.localdomain>
2019-12-15 18:18:02 -0500

    Restore omnibox and right-click search functionality

--- js/background.js
+++ js/background.js
@@ -1,7 +1,168 @@
+// Function for detecting system language and setting it to the default search language
+function detectSystemLanguage(data) {
+	var lang = navigator.languages[0]
+	// Cut off the localization part if it exists (e.g. en-US becomes en), to match with Wikipedia's format
+	var n = lang.indexOf('-')
+	lang = lang.substring(0, n != -1 ? n : lang.length)
+	// Check if the language has a Wikipedia
+	if (defaultPrefixArray.includes(lang)) {
+		console.log("Language auto-detected as '" + lang + "' (" + defaultLangArray[defaultPrefixArray.indexOf(lang)] + ")")
+		chrome.storage.local.set({
+			userLanguage: lang
+		}, function () {
+			return true
+		})
+	} else {
+		// Set it to English as default
+		console.log("Could not auto-detect language, defaulting to 'en' (English)")
+		chrome.storage.local.set({
+			userLanguage: 'en'
+		}, function () {
+			return true
+		})
+	}
+}
+
+// Functions for Omnibox Search
+// Derived from OmniWiki (github.com/hamczu/OmniWiki)
+function initializeOmniboxSearch(userLanguage) {
+	var currentRequest = null
+	chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
+		if (currentRequest != null) {
+			currentRequest.onreadystatechange = null
+			currentRequest.abort()
+			currentRequest = null
+		}
+		updateDefaultSuggestion(text)
+		if (text.length > 0) {
+			currentRequest = suggests(text, function (data) {
+				var results = []
+				// When the settings shortcut is enabled, it takes up one of the search results slots
+				// If shortcut is disabled = Show five search results
+				// If shortcut is enabled = Show four search results + shortcut
+				if (localStorage.getItem("shortcut") === "true") {
+					num = 4
+				} else {
+					num = 5
+				}
+				for (var i = 0; i < num; i++) {
+					results.push({
+						content: data[1][i],
+						description: data[1][i]
+					})
+				}
+				if (localStorage.getItem("shortcut") === "true") {
+					results.push({
+						content: "settings",
+						description: "<dim>Change search language (currently set to " + userLanguage + ")</dim>"
+					})
+				}
+				suggest(results)
+			})
+		}
+	})
+
+	function resetDefaultSuggestion() {
+		chrome.omnibox.setDefaultSuggestion({
+			description: ' '
+		})
+	}
+
+	resetDefaultSuggestion()
+	var searchLabel = chrome.i18n.getMessage('search_label')
+	function updateDefaultSuggestion(text) {
+		chrome.omnibox.setDefaultSuggestion({
+			description: searchLabel + 'Search on Wikipedia: %s'
+		})
+	}
+
+	chrome.omnibox.onInputStarted.addListener(function () {
+		updateDefaultSuggestion('')
+	})
+
+	chrome.omnibox.onInputCancelled.addListener(function () {
+		resetDefaultSuggestion()
+	})
+
+	function suggests(query, callback) {
+		var req = new XMLHttpRequest()
+
+		req.open("GET", "https://" + userLanguage + ".wikipedia.org/w/api.php?action=opensearch&namespace=0&suggest=&search=" + query, true)
+		req.onload = function () {
+			if (this.status == 200) {
+				try {
+					callback(JSON.parse(this.responseText))
+				} catch (e) {
+					this.onerror()
+				}
+			} else {
+				this.onerror()
+			}
+		}
+		req.onerror = function () {
+
+		}
+		req.send()
+	}
+
+	chrome.omnibox.onInputEntered.addListener(function (text) {
+		if (text == "settings") {
+			chrome.tabs.update(null, { url: chrome.extension.getURL('settings.html') })
+		} else {
+			chrome.tabs.update(null, { url: "https://" + userLanguage + ".wikipedia.org/w/index.php?search=" + text })
+		}
+	})
+
+}
+
 chrome.runtime.onInstalled.addListener(function () {
-	// Show welcome page after update
-	// TODO: Verify language function is complete before opening welcome page
-	chrome.storage.local.get(function (data) {
+	// Initialize language settings
+	var langPromise = new Promise(function (resolve, reject) {
+		chrome.storage.local.get(async function (data) {
+			if (data.userLanguage) {
+				console.log("Language already set to '" + data.userLanguage + "' (" + defaultLangArray[defaultPrefixArray.indexOf(data.userLanguage)] + ")")
+				resolve(data)
+			} else {
+				if (localStorage['language']) {
+					// Transfer language setting from Wikipedia Search 7.0.2-9.1
+					if (defaultPrefixArray.includes(localStorage['language'])) {
+						chrome.storage.local.set({
+							userLanguage: localStorage['language']
+						}, function () {
+							console.log('Language setting (' + localStorage['language'] + ') migrated to chrome.storage.')
+							// Delete old variable so this check doesn't happen again
+							localStorage.removeItem('language')
+							resolve(data)
+						})
+					} else {
+						// Detect system language and set it as the default
+						detectSystemLanguage(data)
+						resolve(data)
+					}
+				} else {
+					// Detect system language and set it as the default
+					detectSystemLanguage(data)
+					resolve(data)
+				}
+			}
+		})
+	})
+	// Finish startup
+	Promise.all([langPromise]).then(function (data) {
+		data = data[0]
+		// Initialize Context Menu Search
+		chrome.contextMenus.create({
+			title: "Search Wikipedia for \"%s\"",
+			contexts: ["selection"],
+			onclick: function searchText(info) {
+				chrome.storage.local.get(function (data) {
+					var url = 'https://' + data.userLanguage + '.wikipedia.org/w/index.php?title=Special:Search&search=' + encodeURIComponent(info.selectionText)
+					chrome.tabs.create({ url: url })
+				})
+			}
+		})
+		// Initialize Omnibox search
+		initializeOmniboxSearch(data.userLanguage)
 		// Show welcome page after update
 		if (data.version) {
 			if (!(data.version === chrome.runtime.getManifest().version)) {
@@ -17,151 +178,7 @@ chrome.runtime.onInstalled.addListener(function () {
 			})
 		}
 	})
-	// Initialize language settings
-	chrome.storage.local.get(function (data) {
-		if (data.userLanguage) {
-			console.log("Language already set to '" + data.userLanguage + "' (" + defaultLangArray[defaultPrefixArray.indexOf(data.userLanguage)] + ")")
-		} else {
-			if (localStorage['language']) {
-				// Transfer language setting from Wikipedia Search 7.0.2-9.1
-				if (defaultPrefixArray.includes(localStorage['language'])) {
-					chrome.storage.local.set({
-						userLanguage: localStorage['language']
-					}, function () {
-						// Delete old variable so this check doesn't happen again
-						localStorage.removeItem('language')
-					})
-				}
-			} else {
-				// Detect system language and set it as the default
-				var lang = navigator.languages[0]
-				// Cut off the localization part if it exists (e.g. en-US becomes en), to match with Wikipedia's format
-				var n = lang.indexOf('-')
-				lang = lang.substring(0, n != -1 ? n : lang.length)
-				// Check if the language has a Wikipedia
-				if (defaultPrefixArray.includes(lang)) {
-					console.log("Language auto-detected as '" + lang + "' (" + defaultLangArray[defaultPrefixArray.indexOf(lang)] + ")")
-					chrome.storage.local.set({
-						userLanguage: lang
-					})
-				} else {
-					// Set it to English as default
-					console.log("Could not auto-detect language, defaulting to 'en' (English)")
-					chrome.storage.local.set({
-						userLanguage: 'en'
-					})
-				}
-			}
-		}
-	})
-
-	// Create Context Menu Search
-	chrome.contextMenus.create({
-		title: "Search Wikipedia for \"%s\"",
-		contexts: ["selection"],
-		onclick: function searchText(info) {
-			chrome.storage.local.get(function (data) {
-				var url = 'https://' + data.userLanguage + '.wikipedia.org/w/index.php?title=Special:Search&search=' + encodeURIComponent(info.selectionText)
-				chrome.tabs.create({ url: url })
-			})
-		}
-	})
 })
 
 // Omnibox Search
 // Derived from OmniWiki (github.com/hamczu/OmniWiki)
-var currentRequest = null
-chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-	if (currentRequest != null) {
-		currentRequest.onreadystatechange = null
-		currentRequest.abort()
-		currentRequest = null
-	}
-	updateDefaultSuggestion(text)
-	if (text.length > 0) {
-		currentRequest = suggests(text, function (data) {
-			var results = []
-			// When the settings shortcut is enabled, it takes up one of the search results slots
-			// If shortcut is disabled = Show five search results
-			// If shortcut is enabled = Show four search results + shortcut
-			if (localStorage.getItem("shortcut") === "true") {
-				num = 4
-			} else {
-				num = 5
-			}
-			for (var i = 0; i < num; i++) {
-				results.push({
-					content: data[1][i],
-					description: data[1][i]
-				})
-			}
-			if (localStorage.getItem("shortcut") === "true") {
-				results.push({
-					content: "settings",
-					description: "<dim>Change search language (currently set to " + localStorage["full-language"] + ")</dim>"
-				})
-			}
-			suggest(results)
-		})
-	}
-})
-
-function resetDefaultSuggestion() {
-	chrome.omnibox.setDefaultSuggestion({
-		description: ' '
-	})
-}
-
-resetDefaultSuggestion()
-var searchLabel = chrome.i18n.getMessage('search_label')
-function updateDefaultSuggestion(text) {
-	chrome.omnibox.setDefaultSuggestion({
-		description: searchLabel + 'Search on Wikipedia: %s'
-	})
-}
-
-chrome.omnibox.onInputStarted.addListener(function () {
-	updateDefaultSuggestion('')
-})
-
-chrome.omnibox.onInputCancelled.addListener(function () {
-	resetDefaultSuggestion()
-})
-
-function suggests(query, callback) {
-	var language = localStorage["language"]
-	var protocol = localStorage["protocol"]
-	var req = new XMLHttpRequest()
-
-	req.open("GET", protocol + language + ".wikipedia.org/w/api.php?action=opensearch&namespace=0&suggest=&search=" + query, true)
-	req.onload = function () {
-		if (this.status == 200) {
-			try {
-				callback(JSON.parse(this.responseText))
-			} catch (e) {
-				this.onerror()
-			}
-		} else {
-			this.onerror()
-		}
-	}
-	req.onerror = function () {
-
-	}
-	req.send()
-}
-
-chrome.omnibox.onInputEntered.addListener(function (text) {
-	if (text == "settings") {
-		chrome.tabs.update(null, { url: chrome.extension.getURL('settings.html') })
-	} else {
-		chrome.tabs.update(null, { url: localStorage["protocol"] + localStorage["language"] + ".wikipedia.org/w/index.php?search=" + text })
-	}
-})
-
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-	if (request.method == "getLocalStorage")
-		sendResponse({ data: localStorage[request.key] })
-	else
-		sendResponse({}) // snub them.
-})
