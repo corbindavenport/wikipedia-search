e7ded453a48b21e53527c6b8be10009afa8bee94
Corbin Davenport <corbin@pop-os.localdomain>
2019-12-22 19:20:22 -0500

    Fixed bug where search bar on wikipedia.org would lose focus (#10)

--- js/contentscript.js
+++ js/contentscript.js
@@ -1,5 +1,8 @@
 // Remind users of Wikipedia Search on Wikipedia home page
 if (window.location.href.includes("www.wikipedia.org") && document.body.querySelector(".search-container")) {
-	var searchcontainer = document.body.querySelector(".search-container")
-	searchcontainer.innerHTML = searchcontainer.innerHTML + "You can search Wikipedia from your browser's search bar. Just type 'wiki' in the search bar, then a space, then the name of the article you want."
+	var searchContainer = document.body.querySelector(".search-container")
+	// Create message
+	var message = document.createElement('div')
+	message.innerText = "You can search Wikipedia from your browser's search bar. Just type 'wiki' in the search bar, then a space, then the name of the article you want."
+	searchContainer.appendChild(message)
 }
